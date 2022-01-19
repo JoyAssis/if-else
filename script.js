9426dd21654d1cfc1ac8c1a6600428de74f4154b
Joy <joyceassis96@gmail.com>
2022-01-18 23:02:38 -0300

    Prática 1 e 2

---
+++ script.js
@@ -0,0 +1,16 @@
+// Crie uma condição que exiba uma mensagem no console se a idade for maior que 18 *//
+let idade = `25`
+if ( idade > 18) {
+    console.log ("Yay você é maior de idade")}
+else {
+    console.log ("volte quando for maior")
+}   
+
+//Crie uma condição que exiba uma mensagem no console se você for maior de idade E a variável "humano" seja true *//
+
+let humano = true
+if ( idade >= 18 && humano == true) {
+    console.log ("você é um humano e maior de idade")}
+else {
+    console.log ("você não é maior de idade")
+}   
